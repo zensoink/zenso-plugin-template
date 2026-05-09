8e4ce9dba15cc37e6be172a6f579c966d9c26754
Konrad Stępień <kord.stp@gmail.com>
2026-05-09 15:56:28 +0200

    build: restructure directories, update build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,64 @@
+import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vituum from 'vituum'
 import liquid from '@vituum/vite-plugin-liquid'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export default {
-  plugins: [
-    vituum(),
-    liquid({
-      root: './src',
-      data: ['./src/mock/*.json', './manifest.json'],
-    }),
-  ],
+const paths = {
+  src: './src',
+  dev: './dev',
+  dist: 'dist',
+  cssFile: 'src/main.css'
 }
+
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+  const plugins = []
+
+  if (isDev) {
+    plugins.push(
+      vituum({
+        pages: {
+          dir: `${paths.dev}/pages`,
+          root: paths.src,
+        },
+      }),
+      liquid({
+        root: paths.src,
+        data: [`${paths.dev}/mock/*.json`, './manifest.json'],
+      })
+    )
+  }
+
+  plugins.push(
+    viteStaticCopy({
+      targets: [
+        { src: [`${paths.src}/**/*`, `!${paths.src}/**/*.css`], dest: '.' },
+        { src: ['manifest.json', 'README.md', 'LICENSE', 'CHANGELOG.md', './assets/*'], dest: '.' }
+      ]
+    })
+  )
+
+  return {
+    plugins,
+    build: {
+      outDir: paths.dist,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: resolve(__dirname, `${paths.src}/main.css`),
+        output: {
+          assetFileNames: (assetInfo) =>
+            assetInfo.originalFileName === paths.cssFile
+                ? assetInfo.originalFileName
+                : 'assets/[name]-[hash][extname]'
+        }
+      }
+    },
+    server: {
+      open: '/'
+    }
+  }
+})
