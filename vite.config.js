169cd2e2b3d9259d43587d2945178865bb9caf74
Konrad Stępień <kord.stp@gmail.com>
2026-06-04 13:48:25 +0200

    refactor: centralize styles, update paths, and improve asset processing

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,11 @@ import manifest from './manifest.json' with { type: 'json' }
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const paths = {
-  src: './src',
-  dev: './dev',
+  src: 'src',
+  dev: 'dev',
   dist: 'dist',
-  cssFile: 'src/main.css'
+  cssFile: 'public/main.css',
+  copyTargets: ['manifest.json', 'README.md', 'LICENSE', 'CHANGELOG.md']
 }
 
 export default defineConfig(({ command }) => {
@@ -38,8 +39,8 @@ export default defineConfig(({ command }) => {
   plugins.push(
     viteStaticCopy({
       targets: [
-        { src: [`${paths.src}/**/*`, `!${paths.src}/**/*.css`], dest: '.' },
-        { src: ['manifest.json', 'README.md', 'LICENSE', 'CHANGELOG.md', './assets/*'], dest: '.' }
+        { src: `${paths.src}/**/*.liquid`, dest: '.', rename: { stripBase: 1 } },
+        { src: paths.copyTargets, dest: '.' }
       ]
     })
   )
@@ -50,11 +51,11 @@ export default defineConfig(({ command }) => {
       outDir: paths.dist,
       emptyOutDir: true,
       rollupOptions: {
-        input: resolve(__dirname, `${paths.src}/main.css`),
+        input: resolve(__dirname, `${paths.cssFile}`),
         output: {
           assetFileNames: (assetInfo) =>
-            assetInfo.originalFileName === paths.cssFile
-                ? assetInfo.originalFileName
+             assetInfo.originalFileName === paths.cssFile
+                ? assetInfo.name
                 : 'assets/[name]-[hash][extname]'
         }
       }
